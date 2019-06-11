ad1e8be2eb19f9208043b0e8c2d570702a58e91c
knjo <kazinoshin.jo@gmail.com>
2019-06-11 18:15:33 +0600

    Done with Event Handling Revisited

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,64 +1,21 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
-const History = (props) => {
-    if(props.allClicks.length === 0) {
-        return (
-            <div>
-                This app is used by clicking the buttons.
-            </div>
-        )
-    }
-
-    return (
-        <div>
-            Button press history: {props.allClicks.join(' ')}
-        </div>
-    )
-}
-
-const Button = ({ handleClick, text }) => {
-    return (
-        <button onClick={handleClick}>
-            {text}
-        </button>
-    )
-}
-
 const App = () => {
-    const [left, setLeft] = useState(0);
-    const [right, setRight] = useState(0);
-    const [allClicks, setAll] = useState([])
+    const [value, setValue] = useState(10);
 
-    const handleLeftClick = () => {
-        setAll(allClicks.concat('L'))
-        setLeft(left + 1);
-    };
-
-    const handleRightClick = () => {
-        setAll(allClicks.concat('R'))
-        setRight(right + 1);
-    };
-
-    const handleReset = () => {
-        setAll([])
-        setLeft(0)
-        setRight(0)
+    const hello = (who) => () => {
+        console.log('hello', who)
     }
 
     return (
         <div>
-            <div>
-                {left}
-                <Button handleClick={handleLeftClick} text='left'/>
-                <Button handleClick={handleRightClick} text='right'/>
-                {right}
-                <Button handleClick={handleReset} text='reset'/>
-                <History allClicks={allClicks} />
-            </div>
-            
+            {value}
+            <button onClick={hello('world')}>button</button>
+            <button onClick={hello('react')}>button</button>
+            <button onClick={hello('function')}>button</button>
         </div>
     );
 };
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById('root'))
