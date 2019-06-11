1828020b7967a596b63f83eeb21b30cf3c711b3c
knjo <kazinoshin.jo@gmail.com>
2019-06-11 17:33:47 +0600

    Done with Handling Arrays

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,46 +1,33 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
-const Display = ({ counter }) => {
-  return <div>{counter}</div>;
-};
+const App = () => {
+    const [left, setLeft] = useState(0);
+    const [right, setRight] = useState(0);
+    const [allClicks, setAll] = useState([])
 
-const Button = ({ handleClick, text }) => {
-  return <button onClick={handleClick}>{text}</button>;
-};
-
-const App = props => {
-  const [clicks, setClicks] = useState({
-    left: 0,
-    right: 0
-  });
-
-  const handleLeftClick = () => {
-    const newClicks = {
-      ...clicks,
-      left: clicks.left + 1
+    const handleLeftClick = () => {
+        setAll(allClicks.concat('L'))
+        setLeft(left + 1);
     };
-    setClicks(newClicks);
-  };
 
-  const handleRightClick = () => {
-    const newClicks = {
-      ...clicks,
-      right: clicks.right + 1
+    const handleRightClick = () => {
+        setAll(allClicks.concat('R'))
+        setRight(right + 1);
     };
-    setClicks(newClicks);
-  };
 
-  return (
-    <div>
-      <div>
-        {clicks.left}
-        <button onClick={handleLeftClick}>left</button>
-        <button onClick={handleRightClick}>right</button>
-        {clicks.right}
-      </div>
-    </div>
-  );
+    return (
+        <div>
+            <div>
+                {left}
+                <button onClick={handleLeftClick}>left</button>
+                <button onClick={handleRightClick}>right</button>
+                {right}
+                <p>{allClicks.join(' ')}</p>
+            </div>
+            
+        </div>
+    );
 };
 
 ReactDOM.render(<App />, document.getElementById("root"));
