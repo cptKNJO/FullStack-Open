4c9a88bb47b0c084183bb6cb66d9164175846b48
Kazi Noshin J.O <kazinoshin.jo@gmail.com>
2019-11-04 19:40:34 +0600

    Completed exercise 4.5

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -9,7 +9,21 @@ const totalLikes = blogs => {
   return blogs.reduce(reducer, 0);
 };
 
+const favouriteBlog = blogs => {
+  if (blogs.length === 0) return {};
+
+  const reducer = (mostLiked, current) => (
+    mostLiked.likes > current.likes ? mostLiked : current
+  );
+  let mostLiked = blogs.reduce(reducer, blogs[0]);
+  delete mostLiked._id;
+  delete mostLiked.url;
+  delete mostLiked.__v;
+  return mostLiked;
+};
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favouriteBlog
 };
