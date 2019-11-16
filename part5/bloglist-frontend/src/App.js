b0c15f3f728643754374fb1d37f5e9cd80f4ec52
Kazi Noshin J.O <kazinoshin.jo@gmail.com>
2019-11-16 13:24:02 +0600

    Completed exercise 5.8

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -145,7 +145,7 @@ const App = () => {
           <br></br>
           <br></br>
           <div>
-            {blogs.map(blog => (
+            {blogs.sort((a, b) => b.likes - a.likes).map(blog => (
               <Blog
                 key={blog.id}
                 blog={blog}
